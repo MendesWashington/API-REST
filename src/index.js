751f1632af3d34c1c3eb31fe7080ccc23918e16f
mendesswashington <mendesswashington@gmail.com>
2019-07-24 16:02:06 -0300

    Foi removido a chave secreta da API.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const port = 3000;
+const port = 4000;
 const hostname = 'localhost';
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
